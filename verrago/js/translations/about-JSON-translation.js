ef27b25738bcb2ee3df4ba5d2934eaa7f033f7c8
Abdelhamid Ait-ayoub <abdelhamidaitayoub@gmail.com>
2021-10-10 17:14:24 +0100

    ✏️ Fix Typos

--- verrago/js/translations/about-JSON-translation.js
+++ verrago/js/translations/about-JSON-translation.js
@@ -51,9 +51,19 @@
             "target": "Send Message"
         },
         {
-            "source": "Verrago est une entreprise spécialisée dans la fourniture industrielle opérant dans le marché africain depuis plusieurs années. Forte de ses nombreuses expériences dans les secteurs Oil, Gas, Mines et autres, partenaire des plus grands noms dans ces domaines, Verrago accompagne ses clients et leur offre des solutions clés en main en termes d’amélioration des de leurs machines, équipements, installations et processus. Plusieurs familles de produits, une vaste gamme industriels, des milliers d’articles, des conseils judicieux de nos experts ingénieurs, une approche de proximité, un accompagnement technique, un service après-vente et maintenance 24/7, et bien d’autres avantages mis à votre disposition pour répondre rapidement et efficacement à vos besoins.",
+            "source": "Verrago est une entreprise spécialisée dans la fourniture industrielle opérant dans le marché africain depuis plusieurs années.",
 
-            "target": "Verrago is a company specialized in industrial supply operating in the Africanmarket for several years.With a wealth of experience in the Oil, Gas, Mining and other sectors, and as apartneroffers them turnkey solutions in terms of improving the performance of theirmachines, equipment, installations and processes.Several product families, a wide range of industrial tools, thousands of items,sound advice from our expert engineers, a local approach, technical support,24/7 after-sales and maintenance services, and many other advantages areavailable to you to respond quickly and efficiently to your needs."
+            "target": "Verrago is a company specialized in industrial supply operating in the African market for several years."
+        },
+        {
+            "source": "Forte de ses nombreuses expériences dans les secteurs Oil, Gas, Mines et autres, partenaire des plus grands noms dans ces domaines, Verrago accompagne ses clients et leur offre des solutions clés en main en termes d’amélioration des de leurs machines, équipements, installations et processus.",
+
+            "target": "With a wealth of experience in the Oil, Gas, Mining and other sectors, and as a partner of the biggest names in these fields, Verrago supports its customers and offers them turnkey solutions in terms of improving the performance of their machines, equipment, installations and processes."
+        },
+        {
+            "source": "Plusieurs familles de produits, une vaste gamme industriels, des milliers d’articles, des conseils judicieux de nos experts ingénieurs, une approche de proximité, un accompagnement technique, un service après-vente et maintenance 24/7, et bien d’autres avantages mis à votre disposition pour répondre rapidement et efficacement à vos besoins.",
+
+            "target": "Several product families, a wide range of industrial tools, thousands of items, sound advice from our expert engineers, a local approach, technical support, 24/7 after-sales and maintenance services, and many other advantages are available to you to respond quickly and efficiently to your needs."
         },
         {
             "source": "A propos",
