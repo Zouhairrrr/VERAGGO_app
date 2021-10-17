ab197b3c277e90173eec5bdadeb9cab8783d1fd5
Zouhairrrr <77061268+Zouhairrrr@users.noreply.github.com>
2021-10-17 18:09:21 +0100

    form contact us

--- verrago/form.php
+++ verrago/form.php
@@ -1,11 +1,9 @@
 <?php
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 
 if (isset($_POST['submit'])) {
-
     $message = $_POST['message'];
     $email = $_POST['email'];
     require 'vendor/autoload.php';
@@ -15,22 +13,25 @@ if (isset($_POST['submit'])) {
         $mail->Host       = 'hiver.50dh.net';
         $mail->SMTPAuth   = true;
         $mail->Username   = 'contact@verrago.net';
-        $mail->Password   = 'contactemail123';
-        // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;     
+        $mail->Password   = 'contactemail123';    
         $mail->Port       = 465;
-        //Recipients
         $mail->setFrom('contact@verrago.net', 'Contact us');
-        $mail->addAddress('commercial@verrago.net', 'Commercial');
+        $mail->addAddress('contact@verrago.net', 'Contact Us');
         $mail->addReplyTo('contact@verrago.net', 'Information');
         $mail->isHTML(true);
         $mail->Subject = 'Message sent from the website.';
         $mail->Body    = "$email<br><br>$message";
-
         $mail->send();
-
         $sendmail = $mail;
-
-
+        if ($sendmail) {
+            echo "
+            <script type='text/javascript'>
+            alert('welcome');
+            window.history.back();
+            </script>";
+        } else {
+            echo 'The message could not be sent.<br>';
+        }
     } catch (Exception $e) {
         echo 'The message could not be sent.<br>Mailer Error: ', $mail->ErrorInfo;
     }
